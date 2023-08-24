e2d75436750d10302c2a7d0d439f7440b59f13a5
JonathanGuay <1424439@cegepoutaouais.qc.ca>
2023-08-24 11:30:06 -0400

    personne complété

--- model/Personne.php
+++ model/Personne.php
@@ -4,5 +4,25 @@ namespace model;
 
 class Personne
 {
+    public string $nom;
+    public Compte $compte;
+    public int $nbHeureTravaillerJour;
+    public float $salaireHeure;
+    public string $photo;
+    public $listeAnimaux;
+
+    // Constructeur de la classe MonZoo
+    public function __construct(string $nom, Compte $compte, int $nbHeureTravaillerJour, float $salaireHeure, string $photo) {
+        $this->nom = $nom;
+        $this->compte = $compte;
+        $this->nbHeureTravaillerJour = $nbHeureTravaillerJour;
+        $this->salaireHeure = $salaireHeure;
+        $this->photo = $photo;
+        $this->listeAnimaux = array();
+    }
+
+    public function AvancerTemps(){
+
+    }
 
 }
