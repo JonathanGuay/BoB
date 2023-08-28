b517b2f901e0277670d4eeddaa565658bd391279
JonathanGuay <1424439@cegepoutaouais.qc.ca>
2023-08-28 14:20:36 -0400

    ajout de la date courante et création de session et action travailler dans le controleur

--- Controllers/travailler.php
+++ Controllers/travailler.php
@@ -1,5 +1,6 @@
 <?php
 require '../model/Personne.php';
+session_start();
 class travailler{
     private $personne;
 
@@ -9,11 +10,21 @@ class travailler{
 
     public function Travailler() {
         // ajout a l'heure courant
+        $heureCourante = $_SESSION['date'];
+        $heureTravaille = $_SESSION['personne']->nbHeureTravaillerJour;
+        // Créer un intervalle pour ajouter les heures
+        $interval = new DateInterval("PT{$heureTravaille}H"); // PT signifie Période de Temps, H pour heures
+        // Ajouter l'intervalle à la date courante
+        $heureCourante->add($interval);
 
         // Changer l'argent dans le compte bancaire
+        $salaire = $_SESSION['personne']->salaireHeure;
+        $AjoutMontant = $salaire * $heureTravaille;
+        // Fait le dépôt dans le compte
+        $_SESSION['compte']->depot($AjoutMontant);
 
-        // ramener la vue index
-        include '../index.php';
+        // Rediriger vers la vue index après l'action
+        header("Location: ../index.php");
     }
 
 
