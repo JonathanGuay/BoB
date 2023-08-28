19eb997cf01b7691009edc2169caeba1b0cce52c
fabrice sinarinzi <sinarinzifabrice@gmail.com>
2023-08-28 13:46:42 -0400

    Modification des conatructeurs chat et chien

--- model/Chat.php
+++ model/Chat.php
@@ -6,7 +6,7 @@ class Chat extends Animal
 {
     public function __construct(string $nom, bool $afaim, string $son, int $DureeAvantFaim, int $FraisNourriture, DateTime $dernierRepas, string $photo, string $type = "chat")
     {
-        parent::__construct($type);
+        parent::__construct($nom, $afaim, $DureeAvantFaim, $FraisNourriture, $dernierRepas, $photo, $type);
     }
 
     public function son(): string
